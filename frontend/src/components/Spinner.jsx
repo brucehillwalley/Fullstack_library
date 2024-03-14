432221bd59713627e8e81700f0052a8821d79595
BruceHill <brucehillwalley@gmail.com>
2024-03-14 03:56:56 +0300

    spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="animate-ping w-16 h-16 m-8 rounded-full bg-sky-600"></div>
+    <div
+        className="inline-block h-20 w-20 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-neutral-500 motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status">
+        <span
+          className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+        >Loading...</span>
+      </div>
   );
 };
 
