e1ce9f0f3bed14c19f51fcf616f0151444734eb5
BruceHill <brucehillwalley@gmail.com>
2024-05-04 04:08:53 +0300

    homepath

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,21 @@ app.use(cors())
 // Catch async errors:
 require('express-async-errors')
 
+// HomePath:
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to Library API',
+        version: '1.0.0',
+        api: {
+            routes: {
+                book: 'http://127.0.0.1:8000/book',               
+            },
+            contact: 'brucehillwalley@gmail.com'
+        },
+    })
+})
+
 //Routes
 const router= require('./src/route/library.router')
 app.use(router)
